b132d419e5b5bc382bf4f3b508f85c46df9b189f
tank <tank@certifiedcoderz.com>
2022-10-03 10:16:13 -0400

    lastest updates.

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
-BIZ9_CORE_VERSION='3.5.1'
-APP_VERSION='3.5.3'
+BIZ9_CORE_VERSION='3.6.0';
+APP_VERSION='3.5.4'
 APP_ID="19"
 APP_TITLE="BiZ9-Core"
 REPO_URL="git@github.com:bossappz/biz9-core-stable.git";
