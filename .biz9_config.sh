d3a74a28b36923d938799b36787d3d796211ff8d
tank <tank@certifiedcoderz.com>
2023-11-23 20:14:26 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
-BIZ9_CORE_VERSION='6.0.1'
-APP_VERSION='5.2.3'
+BIZ9_CORE_VERSION='6.0.2'
+APP_VERSION='5.2.4'
 APP_ID="19"
 APP_TITLE="BiZ9-Core"
 REPO_URL="git@github.com:bossappz/biz9-core-stable.git";
